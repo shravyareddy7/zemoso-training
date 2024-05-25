01a9169af83658fa6cd9cd9ba05d1910a9467513
shravyareddy7 <shravya.erravally@zemosolabs.com>
2024-05-25 16:59:58 +0530

    js functions assignment added

---
+++ js/assignment1.js
@@ -0,0 +1,30 @@
+//function passed as a parameter
+function add(x,y)
+{
+    return x+y;
+}
+function sub(x,y)
+{
+    return x-y;
+}
+function mainfunc(subfunc,a,b)
+{
+    return subfunc(a,b);
+}
+console.log(mainfunc(add,1,2));
+console.log(mainfunc(sub,9,2));
+
+
+//arrow function 
+
+const func=(first,second)=>{
+    if(first.length>0 && second.length>0){
+        return `${first[0]}${second[0]}`;
+    }
+    else{
+        return "The length of the string should be more than 1";
+    }
+}
+
+console.log(func("Rogers","Waters"));
+console.log(func("","Waters"));
