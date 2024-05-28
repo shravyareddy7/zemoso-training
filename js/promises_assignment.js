d46dc0b52d0bf17f22dd7e01c7fa1a44d2007ad3
shravyareddy7 <shravya.erravally@zemosolabs.com>
2024-05-28 12:20:55 +0530

    added js assignment files

---
+++ js/promises_assignment.js
@@ -0,0 +1,38 @@
+/**
+*
+* Assume we are fetching the data from a rest endpoint in the get data function.
+* we can simulate the same by replacing the setTimeout with fetch api and executing the same in a browser.
+*
+*/
+
+/*function getData(uId) {
+    setTimeout(() => {
+    console.log("Fetched the data!");
+    return "skc@gmail.com";
+    }, 4000);
+    }
+    
+    console.log("start");
+    var email = getData("skc");
+    console.log("Email id of the user id is: " + email);
+    console.log("end");*/
+
+// How do you solve this problem. How can we wait for till the function execution is completed, so that we can have correct email at line 10?
+
+function getData(uId)
+{
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            console.log("Fetched the data!");
+                resolve("skc@gmail.com");    
+        },4000);
+    });
+}
+console.log("start");
+getData("skc").then(email=>{
+    console.log("Email id of the user id is: " + email);
+    console.log("end");
+}).catch(erroe=>{
+    console.log("Error occured");
+})
+
